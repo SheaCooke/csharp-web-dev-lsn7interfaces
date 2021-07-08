d06a3900636778fad29a1b74759b02aa4ca13da0
shea cooke <SheaSCooke@gmail.com>
2021-07-08 16:01:45 -0500

    finished comparing classes based on cost

--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -19,18 +19,34 @@ namespace IceCreamShop
 
             foreach (var i in availableFlavors) //before sort
             {
-                Console.WriteLine(i);
+                Console.WriteLine(i.Name);
             }
 
+            Console.WriteLine("\n\n");
             availableFlavors.Sort(comparer);
 
             foreach (var i in availableFlavors) // after sort 
             {
-                Console.WriteLine(i);
+                Console.WriteLine(i.Name);
             }
 
             // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
             //  field.
+
+            ConeComparer cc = new ConeComparer();
+
+            foreach (var i in availableCones) // before sorting
+            {
+                Console.WriteLine(i.Name + " " + i.Cost);
+            }
+
+            Console.WriteLine("\n\n");
+            availableCones.Sort(cc);
+
+            foreach (var i in availableCones) // after sorting 
+            {
+                Console.WriteLine(i.Name + " " + i.Cost);
+            }
 
             // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
         }
