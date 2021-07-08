2e97b266b86a6c5d1fbc1c170df236e467cd8f77
shea cooke <SheaSCooke@gmail.com>
2021-07-08 15:40:18 -0500

    initial commit

--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -11,13 +11,28 @@ namespace IceCreamShop
             List<Flavor> availableFlavors = menu.Flavors;
             List<Cone> availableCones = menu.Cones;
 
-        // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
-        //  field.
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
 
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+            // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
+            //  field.
+            FlavorComparer comparer = new FlavorComparer();
+
+            foreach (var i in availableFlavors) //before sort
+            {
+                Console.WriteLine(i);
+            }
+
+            availableFlavors.Sort(comparer);
+
+            foreach (var i in availableFlavors) // after sort 
+            {
+                Console.WriteLine(i);
+            }
+
+            // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
+            //  field.
+
+            // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
         }
     }
 }
